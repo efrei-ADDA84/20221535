4d2a5316fc3625372e0eb94c2bf39ae2902be053
ArTranArt <120374187+ArTranArt@users.noreply.github.com>
2024-04-18 18:54:55 +0200

    Update weather_script_TP3.py

--- weather_script_TP3.py
+++ weather_script_TP3.py
@@ -1,39 +1,32 @@
+from flask import Flask, jsonify, request
 import os
 import requests
-from flask import Flask, jsonify
 
 app = Flask(__name__)
-api_key = get.env(api_key)
 
-def get_weather(latitude, longitude, api_key):
-    url = f"http://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={api_key}&units=metric"
-    print("Appel de l'API en cours...")
+def get_weather(latitude, longitude):
+    api_key = os.environ.get('API_KEY')
+    url = f'http://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={api_key}'
+
     response = requests.get(url)
-    data = response.json()
-    
     if response.status_code == 200:
-        weather_info = {
-            "description": data['weather'][0]['description'],
-            "temperature": data['main']['temp'],
-            "humidity": data['main']['humidity'],
-            "wind_speed": data['wind']['speed']
-        }
-        return weather_info
+        data = response.json()
+        weather = data['weather'][0]['description']
+        return weather
     else:
-        return None
+        return "Error: Failed to retrieve weather data"
+    
 
 @app.route('/')
-def weather():
-    latitude = float(os.environ.get('LATITUDE'))
-    longitude = float(os.environ.get('LONGITUDE'))
-    # api_key = os.environ.get('OPENWEATHER_API_KEY')
-    api_key = "442ed94cfe6ecc84c50d15ba360be7ce"
-    weather_data = get_weather(latitude, longitude, api_key)
-    
-    if weather_data:
-        return jsonify(weather_data)
+def weather_api():
+    latitude = request.args.get('lat')
+    longitude = request.args.get('lon')
+    if latitude and longitude:
+        weather = get_weather(latitude, longitude)
+        return jsonify(weather=weather)
     else:
-        return "Failed to fetch weather data.", 500
+        return jsonify(error="Please provide latitude and longitude parameters"), 400
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+    app.run(debug=True, host='0.0.0.0', port=80)
